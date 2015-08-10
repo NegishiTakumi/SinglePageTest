f647361781171316f3e84553b937cd8df4b7af66
Takumi Negishi <takumi.negishi@o-05882-mac.local>
2015-08-10 15:47:40 +0900

    first commit

--- SinglePageTest/OriginalTabBarController.swift
+++ SinglePageTest/OriginalTabBarController.swift
@@ -6,4 +6,33 @@
 //  Copyright (c) 2015年 Takumi Negishi. All rights reserved.
 //
 
-import Foundation
+import UIKit
+
+class OriginalTabBarController: UITabBarController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+        
+        // あらかじめ色とフォントファミリーを指定
+        let colorKey = UIColor(red: 255/255, green: 158/255, blue: 35/255, alpha: 1.0)
+        let colorBg = UIColor(red: 238/255, green: 238/255, blue: 238/255, alpha: 1.0)
+        let fontFamily: UIFont! = UIFont(name: "Hiragino Kaku Gothic ProN",size:10)
+        
+        // 文字色とフォント変えたい
+        let selectedAttributes: NSDictionary! = [NSFontAttributeName: fontFamily, NSForegroundColorAttributeName: colorKey]
+//        UITabBarItem.appearance().setTitleTextAttributes(selectedAttributes, forState: UIControlState.Selected)
+        
+        // アイコンの色変えたい
+        UITabBar.appearance().tintColor = colorKey
+        
+        // 背景色変えたい
+        UITabBar.appearance().barTintColor = colorBg
+        
+    }
+    
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+}
--- SinglePageTest/ViewController.swift
+++ SinglePageTest/ViewController.swift
@@ -9,7 +9,25 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    @IBOutlet weak var myLabel: UILabel!
+    @IBOutlet weak var OnOffButton: UIButton!
+    var nowPowering : Bool = false;
+    let OffImage = UIImage(named: "OffButton.png")
+    let OnImage = UIImage(named: "OnButton.png")
+    
+    @IBAction func click(sender: UIButton) {
+        if nowPowering {
+            myLabel.text = "電源がOFFになりました"
+            OnOffButton.setImage(OffImage, forState: .Normal)
+            nowPowering = false
+        }else{
+            myLabel.text = "電源がONになりました"
+            OnOffButton.setImage(OnImage, forState: .Normal)
+            nowPowering = true
+        }
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
